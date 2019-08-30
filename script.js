3ef4347357d09948d1f871c350d1743ea76c4a7d
rayremedi <ray.remedi17@laposte.net>
2019-08-30 15:03:50 +0200

    scd commit

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-var apiDestinations = "https://api-ratp.pierre-grimaud.fr/v4/destinations/metros/4";
-var apiDestinations2 = "https://api-ratp.pierre-grimaud.fr/v4/destinations/metros/2";
 
 var apiLignes = "https://api-ratp.pierre-grimaud.fr/v4/lines/metros";
+
+// Variable Station Alesia ligne 4
+
+var apiDestinations = "https://api-ratp.pierre-grimaud.fr/v4/destinations/metros/4";
 var apiHorairesR = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/alesia/R"
 var apiHorairesA = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/alesia/A"
-
 var apiStation = "https://api-ratp.pierre-grimaud.fr/v4/stations/metros/4"
 
 var code = document.querySelector("#code");
 var direction = document.querySelector("#direction");
 var direction2 = document.querySelector("#direction2");
+
+//Station Barbes Rochechouart ligne 2
+var apiDestinations2 = "https://api-ratp.pierre-grimaud.fr/v4/destinations/metros/2";
+var apiHoraires2R = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/2/barbes-rochechouart/R"
+var apiHoraires2A = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/2/barbes-rochechouart/A"
+var apiStation2 = "https://api-ratp.pierre-grimaud.fr/v4/stations/metros/2"
 
 //Fetch les datas de l'API
 
@@ -38,7 +45,7 @@ function callApi() {
   })
   .then(response=>response.json())
   .then(data=>{
-      console.log(data);
+    //   console.log(data);
       horaireA.innerHTML = data.result.schedules[0].message;
       horaireA2.innerHTML = data.result.schedules[1].message;
      
@@ -48,7 +55,7 @@ function callApi() {
 })
 .then(response=>response.json())
 .then(data=>{
-    console.log(data);
+    // console.log(data);
     horaireR.innerHTML = data.result.schedules[0].message;
     horaireR2.innerHTML = data.result.schedules[1].message;
    
@@ -58,10 +65,10 @@ function callApi() {
       })
       .then(response=>response.json())
       .then(data=>{
-          console.log(data);
+        //   console.log(data);
           station.innerHTML =  data.result.stations[24].name;
       })
-          // LIGNE 4 ////////////////////////////////////////////////////////////////////////////////
+          // LIGNE 2 ////////////////////////////////////////////////////////////////////////////////
           fetch(apiDestinations2, {
             method: "get"
           })
@@ -77,7 +84,35 @@ function callApi() {
             .then(response=>response.json())
             .then(data=>{
               //   console.log(data);
-                codeLigne.innerHTML = "M " + data.result.metros[2].code;
+                codeLigne2.innerHTML = "M " + data.result.metros[2].code;
+            })
+            fetch(apiHoraires2R,{
+                method:"get"
+            })
+            .then(response=>response.json())
+            .then(data=>{
+                // console.log(data);
+                horaire2A.innerHTML = data.result.schedules[0].message;
+                horaire2A2.innerHTML = data.result.schedules[1].message;
+               
+            })
+            fetch(apiHorairesA,{
+                method:"get"
+            })
+            .then(response=>response.json())
+            .then(data=>{
+                // console.log(data);
+                horaire2R.innerHTML = data.result.schedules[0].message;
+                horaire2R2.innerHTML = data.result.schedules[1].message;
+               
+            })
+            fetch(apiStation2,{
+                method:"get"
+            })
+            .then(response=>response.json())
+            .then(data=>{
+                // console.log(data);
+                station2.innerHTML =  data.result.stations[12].name;
             })
 }
 
