c3da9d7840be155369f554c363a3af2d0339109a
rayremedi <ray.remedi17@laposte.net>
2019-08-30 14:16:10 +0200

    scd commit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var apiDestinations = "https://api-ratp.pierre-grimaud.fr/v4/destinations/metros/4";
+var apiDestinations2 = "https://api-ratp.pierre-grimaud.fr/v4/destinations/metros/2";
+
 var apiLignes = "https://api-ratp.pierre-grimaud.fr/v4/lines/metros";
-var apiHorairesA = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/mairie+de+montrouge/R"
-var apiHorairesR = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/porte+de+clignancourt/R"
+var apiHorairesR = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/alesia/R"
+var apiHorairesA = "https://api-ratp.pierre-grimaud.fr/v4/schedules/metros/4/alesia/A"
+
 var apiStation = "https://api-ratp.pierre-grimaud.fr/v4/stations/metros/4"
 
 var code = document.querySelector("#code");
@@ -11,24 +14,26 @@ var direction2 = document.querySelector("#direction2");
 //Fetch les datas de l'API
 
 function callApi() {
+
+    // LIGNE 4 ////////////////////////////////////////////////////////////////////////////////
   fetch(apiDestinations, {
     method: "get"
   })
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-      direction.innerHTML = "direction: " + data.result.destinations[0].name;
-      direction2.innerHTML = "direction: " + data.result.destinations[1].name;
+    //   console.log(data);
+      direction.innerHTML = "direction: " + data.result.destinations[1].name;
+      direction2.innerHTML = "direction: " + data.result.destinations[0].name;
     });
   fetch(apiLignes,{
       method:"get"
   })
   .then(response=>response.json())
   .then(data=>{
-      console.log(data);
+    //   console.log(data);
       codeLigne.innerHTML = "M " + data.result.metros[4].code;
   })
-  fetch(apiHorairesA,{
+  fetch(apiHorairesR,{
       method:"get"
   })
   .then(response=>response.json())
@@ -38,23 +43,42 @@ function callApi() {
       horaireA2.innerHTML = data.result.schedules[1].message;
      
   })
-  fetch(apiHorairesR,{
-      method:"get"
-  })
-  .then(response=>response.json())
-  .then(data=>{
-      console.log(data);
-      horaireR.innerHTML = data.result.schedules[0].message;
-      horaireR2.innerHTML = data.result.schedules[1].message;
-      })
+  fetch(apiHorairesA,{
+    method:"get"
+})
+.then(response=>response.json())
+.then(data=>{
+    console.log(data);
+    horaireR.innerHTML = data.result.schedules[0].message;
+    horaireR2.innerHTML = data.result.schedules[1].message;
+   
+})
       fetch(apiStation,{
           method:"get"
       })
       .then(response=>response.json())
       .then(data=>{
           console.log(data);
-          station.innerHTML =  data.result.stations[0].name;
+          station.innerHTML =  data.result.stations[24].name;
       })
+          // LIGNE 4 ////////////////////////////////////////////////////////////////////////////////
+          fetch(apiDestinations2, {
+            method: "get"
+          })
+            .then(response => response.json())
+            .then(data => {
+            //   console.log(data);
+              directionA.innerHTML = "direction: " + data.result.destinations[1].name;
+              directionR.innerHTML = "direction: " + data.result.destinations[0].name;
+            });
+            fetch(apiLignes,{
+                method:"get"
+            })
+            .then(response=>response.json())
+            .then(data=>{
+              //   console.log(data);
+                codeLigne.innerHTML = "M " + data.result.metros[2].code;
+            })
 }
 
 //Rappeler la fonction active ses paramètres
